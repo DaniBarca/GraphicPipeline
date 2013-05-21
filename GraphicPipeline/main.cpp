d8bd3e9d28a5c4e25c1bd687f5907104f3c33c9b
Dani Barca <danibarca@ccpn-41-217.eduroam.upf.edu>
2013-05-21 17:19:38 +0200

    Initial Commit

---
+++ GraphicPipeline/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GraphicPipeline
+//
+//  Created by Dani Barca on 21/05/13.
+//  Copyright (c) 2013 Dani Barca. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
